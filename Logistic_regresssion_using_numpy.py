b0cdeb3ec8aa67eacc5d844890b315d029243c61
jaivardhan1209 <jaivardhan1209@gmail.com>
2019-01-24 16:01:53 -0600

    generic version on logistic regression
    User need to pass path of datafile, target attribute, feature attribute from command line.

--- Logistic_regresssion_using_numpy.py
+++ Logistic_regresssion_using_numpy.py
@@ -9,6 +9,9 @@ Created on Sat Jan 19 08:53:11 2019
 
 import math
 import numpy as np
+import pandas as pd
+import sys
+from sklearn.model_selection import train_test_split
 
 def sigmoid(x):
     result = np.zeros(len(x))
@@ -22,6 +25,11 @@ def costfunction(X, y, theta):
     datasetCount = len(y)
     return ((y.T @ np.log(sigmoid(X @ theta))) + ((1-y).T @ np.log(1 - sigmoid(X @ theta)))) * (1/datasetCount)
     
+def NormalizeDataset(Z):
+    maxval = Z.max()
+    #mean = np.mean(Z)
+    return (Z) / maxval
+
 
 def GradientDescent(X, y, theta, alpha, num_iters):
     m = len(y)
@@ -35,15 +43,41 @@ def GradientDescent(X, y, theta, alpha, num_iters):
 def predictLabel(X, theta):
     return 1 if sigmoid(X @theta) > 0.5 else 0
 
-X = np.array([[1,4.85,9.63],[1,8.62,3.23],[1,5.43,8.23],[1,9.21,6.34]])
-y = np.array([[1],[0],[1],[0]])
+def performLgRegression(path,targetattribute, featureattributes):
+    # read the data set 
+    df = pd.read_csv(path)
+    
+    # perform this task only ur dataset contains string value (yes / no)
+    Y  = df[targetattribute].map({'yes': 1, 'no': 0})
 
-theta = np.array([[0],[0],[0]])
+    featureattributes = featureattributes.split(',')
+    X = df[featureattributes]
 
-theta = GradientDescent(X,y,theta, 0.5,10)
+    ### Normalize the data set 
+    X = NormalizeDataset(X)
+    
+    ones = np.ones(X.shape[0], int).reshape(-1,1)
+    
+    alpha = .02
+    Y = Y.values
+    Y = Y.reshape(-1,1)
 
-print(theta)
+    input = np.column_stack((ones,X))
 
-testdata = np.array([[1,8.62,3.23]])
-predictedval = predictLabel(testdata, theta)
-print("The predicted class is: ", predictedval)
+    theta = np.zeros((X.shape[1] + 1,1))
+    
+     ## divide the dataset in training / testing dataset
+    X_train, X_test, y_train, y_test = train_test_split(
+        input, Y, test_size=0.33, random_state=42)
+
+    theta = GradientDescent(X_train,y_train,theta, alpha,10)
+    print(theta)
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    targetattribute = sys.argv[2]
+    featureattributes = sys.argv[3]
+    print("path to dataSet: ", path)
+    print("Feature attribute: ", featureattributes)
+    print("Target attribute: ", targetattribute)
+    logisticcoefficient = performLgRegression(path, targetattribute, featureattributes)
