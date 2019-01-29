682dd71f3ead82ce0662dfe15bfd76002051ebf8
jaivardhan1209 <jaivardhan1209@gmail.com>
2019-01-29 09:49:14 -0600

    Update Logistic_regresssion_using_numpy.py
    Code added to find the model accuracy but need to think on how to improve the accuracy with model.

--- Logistic_regresssion_using_numpy.py
+++ Logistic_regresssion_using_numpy.py
@@ -13,6 +13,7 @@ import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
 
+### sigmoidal function f(z) = 1/ 1 + e^z 
 def sigmoid(x):
     result = np.zeros(len(x))
     result = result.reshape(-1,1)
@@ -21,29 +22,51 @@ def sigmoid(x):
         result[i] = val # 1 if val >= 0.5 else 0
     return result
 
+### cost for for logistic regression follow below link for more detail 
+### https://ml-cheatsheet.readthedocs.io/en/latest/logistic_regression.html
 def costfunction(X, y, theta):
     datasetCount = len(y)
     return ((y.T @ np.log(sigmoid(X @ theta))) + ((1-y).T @ np.log(1 - sigmoid(X @ theta)))) * (1/datasetCount)
-    
+
+## when features are of different range, we use normalized function to bring data set in normalized form 
 def NormalizeDataset(Z):
     maxval = Z.max()
     #mean = np.mean(Z)
     return (Z) / maxval
 
-
+### this function calculate the rate at which paramter descent
 def GradientDescent(X, y, theta, alpha, num_iters):
     m = len(y)
+    j_history = np.zeros(num_iters)
     for itr in range(num_iters):
         theta = theta - (X.T @ (sigmoid(X @ theta) - y))*(alpha/m)
-       # cost = costfunction(X,y, theta)
-        #print(cost)
+        cost = costfunction(X,y, theta)
+        j_history[itr] = cost
     #print(j_history)
     return theta
 
+## predictive model which use trained theta and use that to predict new label
 def predictLabel(X, theta):
-    return 1 if sigmoid(X @theta) > 0.5 else 0
+    predictedlabel = sigmoid(X @ theta)
+    new_predictedlabel = [1 if el> .5 else 0 for el in predictedlabel]
+    predictedlabel = np.array(new_predictedlabel)
+    return predictedlabel.reshape(-1,1)
+    
+## This function predict the model accuracy with trained theta
+def findModelAccuracy(theta, X , Y):
+    predictedLabel = predictLabel(X, theta)
+    resultantVector = (predictedLabel - Y)
+    totalerror = np.sum(np.absolute(resultantVector))
+    return ((Y.shape[0] - totalerror) / Y.shape[0]) * 100
+    
+### Main method to generate theta and check accuracy of model on test dataset   
+def performLgRegression(X,Y,learningrate,no_iteration):
+    theta = np.zeros((X.shape[1],1))
+    theta = GradientDescent(X,Y,theta, learningrate,no_iteration)
+    return theta
 
-def performLgRegression(path,targetattribute, featureattributes):
+## module responsible for training of model and tweeking the model to improve accuracy
+def TrainModel(path,targetattribute, featureattributes):
     # read the data set 
     df = pd.read_csv(path)
     
@@ -58,21 +81,21 @@ def performLgRegression(path,targetattribute, featureattributes):
     
     ones = np.ones(X.shape[0], int).reshape(-1,1)
     
-    alpha = .02
+    alpha = 1
     Y = Y.values
     Y = Y.reshape(-1,1)
 
     input = np.column_stack((ones,X))
-
-    theta = np.zeros((X.shape[1] + 1,1))
     
      ## divide the dataset in training / testing dataset
     X_train, X_test, y_train, y_test = train_test_split(
         input, Y, test_size=0.33, random_state=42)
-
-    theta = GradientDescent(X_train,y_train,theta, alpha,10)
-    print(theta)
-
+    
+    trainedTheta = performLgRegression(X_train,y_train,alpha,500)
+    modelAccuracy = findModelAccuracy(trainedTheta,X_test,y_test)
+    print('Model Accuracy: ',modelAccuracy)
+    
+    
 if __name__ == "__main__":
     path = sys.argv[1]
     targetattribute = sys.argv[2]
@@ -80,4 +103,5 @@ if __name__ == "__main__":
     print("path to dataSet: ", path)
     print("Feature attribute: ", featureattributes)
     print("Target attribute: ", targetattribute)
-    logisticcoefficient = performLgRegression(path, targetattribute, featureattributes)
+    ## call the method to train the model
+    TrainModel(path, targetattribute, featureattributes)
